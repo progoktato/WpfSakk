b3175afa79ea41edb18c02dd742a6f9569674f4d
Oktató <oktato.ber@gmail.com>
2024-03-06 14:12:28 +0100

    Add project files.

---
+++ WpfSakk/Jatszma.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSakk
+{
+    public class Lepes
+    {
+        string _lepes;
+        public char Tiszt => _lepes[0];
+    }
+
+    //todo Állapottér reprezentáció kialakítása
+    public class Tabla
+    {
+        const char URES_MEZO = ' ';
+        char[,] _tabla = new char[8, 8];
+
+        public Tabla()
+        {
+            for (int i = 0; i < _tabla.GetLength(0); i++)
+            {
+                for (int j = 0; j < _tabla.GetLength(1); i++)
+                {
+                    _tabla[i, j] = URES_MEZO;
+                }
+            }
+        }
+
+        public void  NyitoAllapotLetrehozasa()
+        {
+            //TODO bábuk felhelyzése
+        }
+
+        public void Lep(String ujLepes)
+        {
+            //hibák vizsgálata
+            throw new Exception();    
+
+
+            //todo léptesse oda a bábut, ahová kell
+        }
+
+        //TODO számon analitika, hasznos lekérdezések, stb.
+    }
+
+
+    public class Jatszma
+    {
+        List<String> lepesek;
+        //todo Állapottér reprezentáció kialakítása V2.0
+
+        /// <summary>
+        /// Üres játék létrehozása
+        /// </summary>
+        public Jatszma()
+        {
+            lepesek = new List<String>();
+        }
+        public Jatszma(String fajlSor)
+        {
+            lepesek = new List<String>();
+            foreach (var item in fajlSor.Trim().Split('\t'))
+            {
+                lepesek.Add(item);
+            }
+        }
+
+        public int LepesekSzama => lepesek.Count();
+
+        public char Nyertes => LepesekSzama % 2 == 0 ? 's' : 'v';
+
+        //public int HuszarokLepesszama => lepesek.Count(lepes => lepes[0] == 'H');
+        public int HuszarokLepesszama => TisztLepesszama('H');
+
+        public int TisztLepesszama(char tisztJele)
+        {
+            return lepesek.Count(lepes => lepes[0] == tisztJele);
+        }
+
+        /// <summary>
+        /// todo: Keresse meg mindkét vezér (királynő) utolsó pozícióját és nézze meg, hogy ott ütötték-e ezt a pozíviót? (vmi x poz)
+        /// </summary>
+        public bool VezertUttotek => true;
+    }
+}
---
+++ WpfSakk/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSakk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Jatszma> jatszmak;
+        public MainWindow()
+        {
+            InitializeComponent();
+            jatszmak = File.ReadAllLines("jatszmak.txt").Select(sor => new Jatszma(sor)).ToList();
+
+            MessageBox.Show($"A huszárok ennyi mezőt haladtak: {jatszmak.Sum(jatszma => jatszma.HuszarokLepesszama) * 4}");
+            MessageBox.Show($"A futók ennyiszer léptek: {jatszmak.Sum(jatszma => jatszma.TisztLepesszama('F'))}");
+
+
+            Tabla aktualisAllapot=new Tabla();
+            aktualisAllapot.NyitoAllapotLetrehozasa();
+            aktualisAllapot.Lep("Hx45");
+            //... sok sok lépés
+
+            //Hogyan néz ki ez vizuálisan
+            Tablakep winTablakep = new Tablakep(aktualisAllapot);
+            winTablakep.ShowDialog();
+
+        }
+    }
+}
